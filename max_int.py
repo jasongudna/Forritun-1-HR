e987ff27989bf27121cbdbb72e2ba914ca8d06eb
jasongudna <jasongudna@hotmail.com>
2019-09-05 14:49:31 +0000

    first commit

---
+++ max_int.py
@@ -0,0 +1,8 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+the_sum = 0
+
+while num_int >=0:
+    print(int(input("Input a number: "))
+          if num_int < 0
+          print("The maximum is", max_int) 
